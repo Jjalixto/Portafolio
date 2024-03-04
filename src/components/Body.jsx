6a7fe2adea4731041e025c1338ea8f16c4f8a04b
Joel <jjalixtoc@gmail.com>
2024-03-04 17:44:16 -0500

    header

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,29 +1,12 @@
-import { useEffect } from "react";
-import { useState } from "react"
 
 export const Body = () => {
 
-    const [theme, setTheme] = useState("ligth");
-
-    useEffect(() => {
-        if(theme === "dark"){
-            document.querySelector('html').classList.add('dark')
-        }else{
-            document.querySelector('html').classList.remove('dark')
-        }
-    },[theme])
-
-    const handleChangeTheme = () => {
-        setTheme(prevTheme => prevTheme === "ligth" ? "dark" : "ligth")
-    }
-
     return (
         <>
-            <div className="h-screen flex justify-center items-center dark:bg-neutral-900">
-                <button className="bg-slate-200 px-4 py-2 rounded hover:bg-slate-300 dark:bg-slate-950 dark:text-white dark:hover:bg-slate-900"
-                onClick={handleChangeTheme}>
-                    Change theme
-                </button>
+            <div className="h-screen flex justify-center items-center bg-cyan-300 dark:bg-neutral-900">
+                <h1 className="font-outfit text-6xl m-3">
+                    Joel Jalixto
+                </h1>
             </div>
         </>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,38 @@
+import { useEffect, useState } from "react";
 
 export const Header = () => {
+    const [theme, setTheme] = useState("ligth");
 
+    useEffect(() => {
+        if(theme === "dark"){
+            document.querySelector('html').classList.add('dark')
+        }else{
+            document.querySelector('html').classList.remove('dark')
+        }
+    },[theme])
+
+    const handleChangeTheme = () => {
+        setTheme(prevTheme => prevTheme === "ligth" ? "dark" : "ligth")
+    }
     return(
         <>
         <section className="bg-white dark:bg-black">
             <div className="container flex justify-between items-center py-4 mx-8 bg-white dark:bg-black">
                 <div className="flex items-center">
-                    <a className="font-serif text-black font-bold text-xl ml-8 space-x-4  dark:text-white">Joel</a>
+                    <a href="/joel" className="hover:text-neutral-500 font-outfit text-black font-bold text-xl ml-8 space-x-4 dark:text-white">Joel</a>
                     <nav className="ml-6 hidden md:block">
-                        <ul className="font-outfit flex space-x-4 items-center text-black dark:text-white">
-                            <li>Inicio</li>
-                            <li>Sobre Mi</li>
-                            <li>Mis Habilidades</li>
-                            <li>Mis Proyectos</li>
+                        <ul className="font-outfit flex space-x-4 items-center text-black">
+                            <li className="hover:text-neutral-500">Inicio</li>
+                            <li className="hover:text-neutral-500">Sobre Mi</li>
+                            <li className="hover:text-neutral-500">Mis Habilidades</li>
+                            <li className="hover:text-neutral-500">Mis Proyectos</li>
                         </ul>
                     </nav>
                 </div>
                 <div className="flex justify-end">
-                    <img src="luz.png" alt="luna" width="35px" height="5px" className="mx-4" />
-                    <button className="text-white cursor-pointer p-2 bg-orange-400 rounded-lg px-4 mx-4 dark:text-white">Contacto</button>
+                    <button onClick={handleChangeTheme} >
+                    <img src="luz.png" alt="luna" width="35px" height="5px" className="mx-4" /></button>
+                    <button className="text-white cursor-pointer p-2 bg-orange-600 rounded-lg px-4 mx-4 hover:bg-orange-400 dark:text-white">Contacto</button>
                 </div>
             </div>
         </section>
